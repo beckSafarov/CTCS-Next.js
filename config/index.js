5413d0cff874f4e1b64d26c95d256d1f5cc1c89b
Begzod Safarov <begzod.safarov07@gmail.com>
2021-06-03 07:31:56 +0800

    production url added

--- config/index.js
+++ config/index.js
@@ -1,6 +1,8 @@
-export const ROOT = !process.env.NEXT_PUBLIC_VERCEL_URL
+const dev = process.env.NODE_ENV !== 'production';
+
+export const ROOT = dev
   ? process.env.NEXT_PUBLIC_ROOT
-  : process.env.NEXT_PUBLIC_VERCEL_URL;
+  : 'http://ctcs-next-js.vercel.app';
 
 export const ABOUT = `${ROOT}/about`;
 export const ACTIVITIES = `${ROOT}/activities`;
